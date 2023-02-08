58119c987709a28a8fa90ef85e658d980c5c3ed2
andrey <aaashvyrevaaa>
2023-02-08 18:41:45 +0300

    Результат отбора

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def promotion_image():
                    alt="здесь должна была быть картинка, но не нашлась"><h1>{'</br>'.join(['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
                                                                                            'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!'])}</h1>'''
 
+
 @app.route('/choice/<name>')
 def choice(name):
     return f'''<!doctype html>
@@ -51,9 +52,36 @@ def choice(name):
            </div>
   </body>
 </html>'''
+
+
 @app.route('/results/<nickname>/<int:level>/<float:rating>')
-def results():
-    pass
+def results(nickname, level, rating):
+    return f'''<!doctype html>
+    <html lang="en">
+      <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+      </head>
+      <body>
+      <h1>Результаты отбора</h2>
+      <h3>Претендента на участие в миссии: {nickname}</h1>
+      <div class="alert alert-primary" role="alert">
+          Поздравляем ваш рейтинг после {level} этапа
+        </div>
+        <div class="alert alert-secondary" role="alert">
+          составляет {rating}
+        </div>
+        <div class="alert alert-danger" role="alert">
+          Желаем удачи
+        </div>
+      </body>
+    </html>'''
+
+
 @app.route('/form_sample', methods=['POST', 'GET'])
 def form_sample():
     if request.method == 'GET':
