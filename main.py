69016b9f5cbc71c71b5d6e774eb226d47e78b2aa
Andrey <aaashvyrevaaa@yandex.ru>
2023-02-11 13:50:55 +0300

    Сверх задания добавил отступ для h5, так красивее

--- main.py
+++ main.py
@@ -32,5 +32,20 @@ def list_prof(param):
         return 'Bad param. Use ul/ol'
 
 
+@app.route('/answer')
+@app.route('/auto_answer')
+def answer():
+    param = {}
+    param['title'] = 'Анкета'
+    param['surname'] = 'Watny'
+    param['name'] = 'Mark'
+    param['education'] = 'выше среднего'
+    param['profession'] = 'штурман марсохода'
+    param['sex'] = 'male'
+    param['motivation'] = 'Всегда мечтал застрять на Марсе!'
+    param['ready'] = 'True'
+    return render_template('auto_answer.html', **param)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
