bffce87d5377424681c1c1bb0716ec29ffc699e9
Andrey <aaashvyrevaaa@yandex.ru>
2023-02-11 14:56:12 +0300

    По каютам!

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
 
 @app.route('/')
 def start():
@@ -64,6 +65,12 @@ def login():
     if form.validate_on_submit():
         return 'Самоуничтожение активировано'
     return render_template('login.html', title='Аварийный доступ', form=form)
+
+
+@app.route('/distribution')
+def distribution():
+    names = ['Ридли Скотт', 'Энди Уир', 'Марк Уотни', 'Венката Капур', 'Тедди Сандрес']
+    return render_template('distribution.html', name=names)
 
 
 if __name__ == '__main__':
