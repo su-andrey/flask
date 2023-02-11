8358a4ff0e430664bb997a7e8459744d2c839d86
Andrey <aaashvyrevaaa@yandex.ru>
2023-02-11 13:30:31 +0300

    Cписок профессий

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def start():
     return 'Nothing to show here, choose another url'
 
+
 @app.route('/index/<name>')
 def name(name):
     return render_template('index.html', title=name)
+
 
 @app.route('/training/<prof>')
 def training(prof):
@@ -16,5 +19,18 @@ def training(prof):
         return render_template('training.html', name='tex.png')
     else:
         return render_template('training.html', name='science.png')
+
+
+@app.route('/list_prof/<param>')
+def list_prof(param):
+    professions = ['штурман', 'пилот', 'экзобиолог', 'инженер - исследователь', 'строитель', 'врач', 'пилот дронов']
+    if param == 'ol':
+        return render_template('list_prof.html', profs=professions, type=param)
+    elif param == 'ul':
+        return render_template('list_prof.html', profs=professions, type=param)
+    else:
+        return 'Bad param. Use ul/ol'
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
