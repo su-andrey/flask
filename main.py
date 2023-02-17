7a066324a11a7302c5493352694fde5a1d85e136
Andrey <aaashvyrevaaa@yandex.ru>
2023-02-17 21:34:40 +0300

    Первая работа

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
 from flask import Flask
-
+import datetime
 from data import db_session
-from data.users import User
+from data.jobs import Jobs
 
 db_session.global_init("db/mars_explorer.db ")
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 db_sess = db_session.create_session()
-info = [['Ridley', 'Scott', 21, 'captain', 'research engineer', 'module_1', 'scott_chief@mars.org', 'cap'],
-        ['Derek', 'Scott', 27, 'mechanic', 'mechanic', 'module_3', 'd.scott@mars.org', 'mech'],
-        ['Lewis', 'Johnson', 58, 'doctor', 'medecine', 'module_2', 'doc_lewis@mars.org', 'doc'],
-        ['Mark', 'Andreas', 34, 'scientist', 'biology', 'module_1', 'mr.anreas@mars.org', 'mab']]
-for elem in info:
-    user = User()
-    user.surname = elem[1]
-    user.name = elem[0]
-    user.age = elem[2]
-    user.position = elem[3]
-    user.speciality = elem[4]
-    user.address = elem[5]
-    user.email = elem[-2]
-    user.hashed_password = elem[-1]
-    db_sess.add(user)
+user = Jobs()
+user.team_leader = 1
+user.job = 'deployment of residential modules 1 and 2'
+user.work_size = 15
+user.collaborators = '2, 3'
+user.start_date = datetime.datetime.now()
+user.is_finished = False
+db_sess.add(user)
 db_sess.commit()
